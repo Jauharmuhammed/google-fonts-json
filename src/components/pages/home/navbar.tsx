d6e34951c33f66a2b44e5bc9709dcdc31da2ca06
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2024-12-25 14:54:21 +0530

    github link added

--- src/components/pages/home/navbar.tsx
+++ src/components/pages/home/navbar.tsx
@@ -6,6 +6,8 @@ import { Download } from "lucide-react";
 import Title from "./title";
 import { downloadJson } from "@/lib/utils";
 import { Loader } from "@/components/ui/loader";
+import { GithubIcon } from "@/components/icons";
+import Link from "next/link";
 
 type Props = {
   data: any;
@@ -28,7 +30,18 @@ function navbar({ data }: Props) {
       <CardContent className="flex items-center justify-between p-2 rounded">
         <Title className="ms-2" />
         <div className="flex items-center gap-2">
-          <ThemeToggle />
+          <div className="flex items-center gap-0.5">
+            <Link target="_blank" href={process.env.NEXT_PUBLIC_GITHUB_URL!}>
+              <Button
+                variant={"ghost"}
+                size={"icon"}
+                className="focus-visible:ring-0"
+              >
+                <GithubIcon className="h-4 w-4" />
+              </Button>
+            </Link>
+            <ThemeToggle />
+          </div>
           <Button onClick={handleDownload} className="flex items-center gap-2">
             {isDownloading ? <Loader /> : <Download className="h-4 w-4" />}
             Download
